909e08e6df1f4850245ea9d58d580c3f92c9cfc9
Editor <editorspageyt@gmail.com>
2024-01-16 19:55:30 +0530

    Nav Button & Margin

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Projects from "./pages/Projects.jsx";
 import Updates from "./pages/Updates.jsx";
+import ContactMe from "./pages/ContactMe.jsx";
 import "./app.css";
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/" element={<Home />}></Route>
           <Route path="/projects" element={<Projects />}></Route>
           <Route path="/updates" element={<Updates />}></Route>
+          <Route path="/contactme" element={<ContactMe />}></Route>
         </Routes>
       </BrowserRouter>
       </div>
---
+++ src/pages/ContactMe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactMe = () => {
+  return (
+    <>
+        <div className='page-content' style={{backgroundColor:"#2374AB"}}>Contact Me</div>
+    </>
+  )
+}
+
+export default ContactMe
